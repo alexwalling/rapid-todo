fd424b62e35098c69e768e8cd0953c4c4e6a7580
Alex Walling <alexwalling@Alexs-MacBook-Pro.local>
2017-06-08 14:46:41 -0700

    day of work

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
+/* 
+required packages
+*/
 const RapidAPI = require('rapidapi-connect');
 const rapid = new RapidAPI("rapidapi-tutorial_5930670fe4b0eaefb644ce16", "99bde403-0d62-4088-b808-6d990e7babde");
 var fs = require('fs');
 var dotenv = require('dotenv');
 
+/*
+loading dev environment for API keys
+*/
 dotenv.load();
 
+/*
+global variables
+*/
 var firstSpace = 0;
 var n = 1;
-var db;
+var db = ['hello\n', 'new phone\n', 'who dis\n'];
 
+/*
+.listen for /command
+*/
 rapid.listen('Slack', 'slashCommand', { 
 	'token': process.env.TOKEN,
 	'command': process.env.CMD,
-	'response': db
+	'response': 'processing'
 })
 	.on('join', () => {
-		 /* YOUR CODE GOES HERE */ 
+
 	})
 	.on('message', (message) => {
-		console.log('env' + process.env.TOKEN);
-		console.log(message.text);
+		var channel = message.channel_id;
+		// calls printNote to init slackbot to post current todo list in the same channel.
+
+
+		//Logging to note txt -----------------------------------------------------------------------------------
 		parseText(message.text, function(res) {
 			console.log(res);
 			if(res == 1){
@@ -31,26 +46,71 @@ rapid.listen('Slack', 'slashCommand', {
 			} else if(res == 2){
 				//remove the line number
 			} else if(res == 3){
-				fs.readFile('note.txt', 'utf8', function read(err, data) {
-					console.log(data);
-				});
+				printNote(channel);
 			}
 		});
-		updateArray();
 	})
 	.on('error', (error) => {
 		console.log(error);
 	})
 	.on('close', (reason) => {
-		 /* YOUR CODE GOES HERE */ 
+
 	});
 
-function getNoteToPrint(cb){
-	fs.readFile('note.txt', 'utf8', function read(err, data) {
-		cb(data);
+/*
+prints current todo list to the same channel that the slash command was called
+
+variables:
+	channel-name: either channel name or channel id
+*/
+function printNote(channel_name){
+	rapid.call('Slack', 'postMessage', { 
+		'token': process.env.TOKEN2,
+		'channel': channel_name,
+		'text': getNoteToPrint(),
+		'parse': '',
+		'linkNames': '',
+		'attachments': '',
+		'unfurlLinks': '',
+		'unfurlMedia': '',
+		'username': 'Rapid Todo List',
+		'asUser': '',
+		'iconUrl': '',
+		'iconEmoji': ':raised_hands:'
+
+	}).on('success', (payload)=>{
+
+	}).on('error', (payload)=>{
+
 	});
+}	
+
+/*
+reads text file to display the current todo list. 
+*/
+function getNoteToPrint(){
+	var res;
+	/*
+	fs.readFile('note.txt', 'utf8', function read(err, data) {
+		console.log(typeof data);
+		res = data;
+	});
+	console.log(res);
+	return 'hello';
+	*/
+
+	var text = fs.readFileSync('note.txt','utf8');
+	console.log(text);
+	return text;
 }
 
+/*
+parses the slash command to decide what to do
+
+variables
+	payload: includes the message to be parsed.
+	cb: callback to return information
+*/
 function parseText(payload, cb) {
 	if(payload != undefined){
 		firstSpace = payload.indexOf(' ');
